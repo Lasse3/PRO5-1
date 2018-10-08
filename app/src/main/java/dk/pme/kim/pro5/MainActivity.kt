fb3bf39f0f7f389d550cf64fdc234832e9e9cd44
Kim Nielsen <kimnielsen33333@gmail.com>
2018-10-08 12:39:31 +0200

    Initial commit

---
+++ app/src/main/java/dk/pme/kim/pro5/MainActivity.kt
@@ -0,0 +1,24 @@
+package dk.pme.kim.pro5
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun btnCollect(view: View)
+    {
+
+    }
+
+    fun btnTransfer(view: View)
+    {
+
+    }
+}
