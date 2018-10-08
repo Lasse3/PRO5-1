5442952bb70ef436e4cb3b6b3fe64cdf4ba4177e
Kim Nielsen <kimnielsen33333@gmail.com>
2018-10-08 13:17:17 +0200

    Added button click listeners

--- app/src/main/java/dk/pme/kim/pro5/MainActivity.kt
+++ app/src/main/java/dk/pme/kim/pro5/MainActivity.kt
@@ -3,6 +3,7 @@ package dk.pme.kim.pro5
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,11 +15,27 @@ class MainActivity : AppCompatActivity() {
 
     fun btnCollect(view: View)
     {
+        if(id_collect.text.toString() == resources.getString(R.string.collectData))
+        {
+            id_collect.setText(resources.getString(R.string.collectingData))
+        }
 
+        else
+        {
+            id_collect.setText(resources.getString(R.string.collectData))
+        }
     }
 
     fun btnTransfer(view: View)
     {
+        if(id_transfer.text.toString().equals(resources.getString(R.string.transferData)))
+        {
+            id_transfer.setText(resources.getString(R.string.transferingData))
+        }
 
+        else
+        {
+            id_transfer.setText(resources.getString(R.string.transferData))
+        }
     }
 }
