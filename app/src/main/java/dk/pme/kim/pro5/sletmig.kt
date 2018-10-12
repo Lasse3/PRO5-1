1d2ecb40567ef1296a4b41c5f70ca09717170ea1
Jesper Hjort <jj.mypro@gmail.com>
2018-10-12 12:21:07 +0200

    sletmig

---
+++ app/src/main/java/dk/pme/kim/pro5/sletmig.kt
@@ -0,0 +1,12 @@
+package dk.pme.kim.pro5
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class sletmig : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sletmig)
+    }
+}
